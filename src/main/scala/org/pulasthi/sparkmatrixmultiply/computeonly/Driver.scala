dbe5c572f66283550c8903ec6474e4318d1c71f0
pulasthi <pulasthi911@gmail.com>
2016-06-19 20:56:00 -0400

    adding code to read preX or to generate it

--- src/main/scala/org/pulasthi/sparkmatrixmultiply/computeonly/Driver.scala
+++ src/main/scala/org/pulasthi/sparkmatrixmultiply/computeonly/Driver.scala
@@ -1,8 +1,11 @@
 package org.pulasthi.sparkmatrixmultiply.computeonly
 
+import java.io.IOException
 import java.nio.{ByteBuffer, ByteOrder}
+import java.util.Random
+import java.util.regex.Pattern
 
-import com.google.common.base.Optional
+import com.google.common.base.{Strings, Optional}
 import edu.indiana.soic.spidal.common.{RangePartitioner, Range}
 import edu.indiana.soic.spidal.spark.damds.ParallelOps
 import org.apache.commons.cli._
@@ -13,6 +16,8 @@ import org.apache.spark.rdd.RDD
 import org.pulasthi.sparkmatrixmultiply.configurations.ConfigurationMgr
 import org.pulasthi.sparkmatrixmultiply.configurations.section.DAMDSSection
 import org.pulasthi.sparkmatrixmultiply.damds._
+
+import scala.io.Source
 
 /**
   * Created by pulasthi on 6/19/16.
@@ -57,6 +62,10 @@ object Driver {
     hdoopconf.set("mapred.min.split.size", ""+blockbtyesize);
     hdoopconf.set("mapred.max.split.size", ""+blockbtyesize);
 
+    var preX: Array[Array[Double]] = if (Strings.isNullOrEmpty(config.initialPointsFile))
+      generateInitMapping(config.numberDataPoints, config.targetDimension)
+    else readInitMapping(config.initialPointsFile, config.numberDataPoints, config.targetDimension);
+
     val ranges: Array[Range] = RangePartitioner.Partition(0, config.numberDataPoints, 1)
     ParallelOps.procRowRange = ranges(0);
     var datardd = sc.binaryRecords(config.distanceMatrixFile,2*config.numberDataPoints,hdoopconf);
@@ -68,6 +77,10 @@ object Driver {
       ByteBuffer.wrap(cur).asShortBuffer().get(shorts)
       shorts
     }}
+
+    //TODO : check if we need to replace 0 with min values
+
+
   }
 
   def readConfigurations(cmd: CommandLine): Unit = {
@@ -98,5 +111,39 @@ object Driver {
       }
     }
     return Optional.fromNullable(null);
+  }
+
+
+  def generateInitMapping(numPoints: Int, targetDim: Int): Array[Array[Double]] = {
+    var x: Array[Array[Double]] = Array.ofDim[Double](numPoints, targetDim);
+    val rand: Random = new Random(System.currentTimeMillis)
+    for (row <- x) {
+      for (i <- 0 until row.length) {
+        row(i) = if (rand.nextBoolean) rand.nextDouble else -rand.nextDouble
+      }
+    }
+    return x;
+  }
+
+  def readInitMapping(initialPointsFile: String, numPoints: Int, targetDimension: Int): Array[Array[Double]] = {
+    try {
+      var x: Array[Array[Double]] = Array.ofDim[Double](numPoints, targetDimension);
+      var line: String = null
+      val pattern: Pattern = Pattern.compile("[\t]")
+      var row: Int = 0
+      for (line <- Source.fromFile(initialPointsFile).getLines()) {
+        if (!Strings.isNullOrEmpty(line)) {
+          val splits: Array[String] = pattern.split(line.trim)
+
+          for (i <- 0 until splits.length) {
+            x(row)(i) = splits(i).trim.toDouble
+          }
+          row += 1;
+        }
+      }
+      return x;
+    } catch {
+      case ex: IOException => throw new RuntimeException(ex)
+    }
   }
 }
