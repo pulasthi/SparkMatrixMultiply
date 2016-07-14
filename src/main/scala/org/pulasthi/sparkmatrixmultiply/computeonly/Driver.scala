29f423006db366b9cab17ca6677c73b0948b4fa4
pulasthi <pulasthi911@gmail.com>
2016-07-14 16:15:44 -0400

    fixing arguments

--- src/main/scala/org/pulasthi/sparkmatrixmultiply/computeonly/Driver.scala
+++ src/main/scala/org/pulasthi/sparkmatrixmultiply/computeonly/Driver.scala
@@ -7,8 +7,6 @@ import org.apache.commons.cli._
 import org.apache.spark.{Accumulator, SparkConf, SparkContext}
 import org.pulasthi.sparkmatrixmultiply.{MMUtils, MatrixUtils}
 import org.pulasthi.sparkmatrixmultiply.configurations.section.DAMDSSection
-import org.pulasthi.sparkmatrixmultiply.damds._
-
 
 /**
   * Created by pulasthi on 6/19/16.
@@ -24,29 +22,30 @@ object Driver {
   var iterationTimer: Stopwatch = Stopwatch.createUnstarted()
   var iterationTime: Long = 0L;
   var mainTimer: Stopwatch = Stopwatch.createUnstarted()
+  var parallism: Int = 1;
 
   def main(args: Array[String]): Unit = {
 
     mainTimer.start();
-    val conf = new SparkConf().setAppName("sparkMDS")
+    val conf = new SparkConf().setAppName("Spark Matrix Multiplication")
     val sc = new SparkContext(conf)
 
     val iterations = args(0).toInt
     val globalColCount = args(1).toInt
-    ParallelOps.nodeCount = args(2).toInt
-    val blockSize = if ((args.length > 3)) args(3).toInt else 64
-    ParallelOps.threadCount = if ((args.length > 4)) args(4).toInt else 1
+    targetDimension = args(2).toInt
+    val blockSize = args(3).toInt
+    var rowcount: Int = args(4).toInt
+    parallism = args(5).toInt;
 
-    val parallism = ParallelOps.nodeCount*ParallelOps.threadCount;
     val taskRowCounts: Array[Int] = Array.ofDim[Int](parallism);
-    calculaterowCounts(globalColCount,taskRowCounts,parallism);
+    calculaterowCounts(rowcount * parallism,taskRowCounts,parallism);
 
     //create RDD to make parallel calls
     val runRDD = sc.parallelize(1 to parallism, parallism);
-    val rowcount = runRDD.mapPartitionsWithIndex(runTask(globalColCount,targetDimension,taskRowCounts, blockSize, iterations)).count()
+    val rowcountEnd = runRDD.mapPartitionsWithIndex(runTask(globalColCount,targetDimension,taskRowCounts, blockSize, iterations)).sum()
     mainTimer.stop();
 
-    println("Total Row Count " + rowcount);
+    println("Total Row Count " + rowcountEnd);
     println("Total Time " + mainTimer.elapsed(TimeUnit.MILLISECONDS));
     println("Iteration Time " + iterationTime);
 
@@ -74,9 +73,7 @@ object Driver {
     MMUtils.generateBofZ(localRowCount,globalColCount,partialBofZ);
 
     for(i <- 0 until iterations){
-      iterationTimer.start();
       MatrixUtils.matrixMultiply(partialBofZ, preX, partialBofZ.length, targetDimension, globalColCount, blockSize, multiplyResult);
-      iterationTimer
     }
 
     println("Task Index " + index + " Number of Rows " + localRowCount)
