054774641f4038392bd503865f172b893b376fd9
pulasthi <pulasthi911@gmail.com>
2016-07-24 16:18:39 -0400

    with bc variables

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "SparkMatrixMultiply"
 
-version := "1.1"
+version := "2.0"
 
 scalaVersion := "2.11.8"
 
--- src/main/scala/org/pulasthi/sparkmatrixmultiply/computeonly/Driver.scala
+++ src/main/scala/org/pulasthi/sparkmatrixmultiply/computeonly/Driver.scala
@@ -4,6 +4,7 @@ import java.nio.{ByteOrder}
 import java.util.concurrent.TimeUnit
 import com.google.common.base.Stopwatch
 import org.apache.commons.cli._
+import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.{Accumulator, SparkConf, SparkContext}
 import org.pulasthi.sparkmatrixmultiply.{MMUtils, MatrixUtils}
 import org.pulasthi.sparkmatrixmultiply.configurations.section.DAMDSSection
@@ -23,6 +24,7 @@ object Driver {
   var iterationTime: Long = 0L;
   var mainTimer: Stopwatch = Stopwatch.createUnstarted()
   var parallism: Int = 1;
+  var multilength: Int = 1;
 
 
   def main(args: Array[String]): Unit = {
@@ -44,13 +46,46 @@ object Driver {
     //create RDD to make parallel calls
     val runRDD = sc.parallelize(1 to parallism, parallism);
     //With matrix results
+    val preX: Array[Double] = Array.ofDim[Double](globalColCount * targetDimension);
+    val partialBofZ: Array[Array[Double]] = Array.ofDim[Double](rowcount,globalColCount);;
+    val multiplyResult: Array[Double] = Array.ofDim[Double](partialBofZ.length*targetDimension);
+
+    MMUtils.generatePreX(globalColCount,targetDimension,preX);
+    MMUtils.generateBofZ(rowcount,globalColCount,partialBofZ);
+    println("Length of result " + multilength);
+
+    val bcPrex = sc.broadcast(preX);
+    val bcpartialBofZ = sc.broadcast(partialBofZ);
+    val bcglobalColCount = sc.broadcast(globalColCount)
+    val bctargetDimension = sc.broadcast(targetDimension)
+    val bctaskRowCounts = sc.broadcast(taskRowCounts)
+    val bcblockSize = sc.broadcast(blockSize)
+    val bciterations = sc.broadcast(iterations)
+
+
+    //with matrix collected
+//    for(i <- 0 until iterations){
+//      var multiplyResult = runRDD.mapPartitionsWithIndex(runTaskCollect(globalColCount,targetDimension,taskRowCounts, blockSize, iterations)).reduce(collectResults)
+//      multilength = multiplyResult.length;
+//    }
+
+
     for(i <- 0 until iterations){
-      val multiplyResult = runRDD.mapPartitionsWithIndex(runTaskCollect(globalColCount,targetDimension,taskRowCounts, blockSize, iterations)).reduce(collectResults)
+        val multiplyResult = runRDD.mapPartitionsWithIndex{ (index: Int, it: Iterator[Int]) =>
+
+          val localRowCount = bctaskRowCounts.value(index);
+          val multiplyResult: Array[Double] = Array.ofDim[Double](bcpartialBofZ.value.length*bctargetDimension.value);
+          MatrixUtils.matrixMultiply(bcpartialBofZ.value, bcPrex.value, bcpartialBofZ.value.length, bctargetDimension.value, bcglobalColCount.value, bcblockSize.value, multiplyResult);
+          return List(localRowCount).iterator;
+
+        }.collect
+      multilength = multiplyResult.length;
     }
     mainTimer.stop();
 
-    println("Total Time for col with matrix reduction :" + globalColCount + " row " + rowcount + " block size " + blockSize + " : " + mainTimer.elapsed(TimeUnit.MILLISECONDS));
+    println("Total Time for col :" + globalColCount + " row " + rowcount + " block size " + blockSize + " : " + mainTimer.elapsed(TimeUnit.MILLISECONDS));
     println("Iteration Time " + iterationTime);
+    println("Length of result " + multilength);
 
   }
 
@@ -66,39 +101,30 @@ object Driver {
 
   }
 
-  def runTask(globalColCount: Int, targetDimension: Int, taskRowCounts: Array[Int], blockSize: Int, iterations: Int)(index: Int, iter: Iterator[Int]) : Iterator[Int] = {
-    val localRowCount = taskRowCounts(index);
-    val preX: Array[Double] = Array.ofDim[Double](globalColCount * targetDimension);
-    val partialBofZ: Array[Array[Double]] = Array.ofDim[Double](localRowCount,globalColCount);;
-    val multiplyResult: Array[Double] = Array.ofDim[Double](partialBofZ.length*targetDimension);
+//  def runTask(globalColCount: Int, targetDimension: Int, taskRowCounts: Array[Int], blockSize: Int, iterations: Int)(index: Int, iter: Iterator[Int]) : Iterator[Int] = {
+//    val localRowCount = taskRowCounts(index);
+//    val multiplyResult: Array[Double] = Array.ofDim[Double](bcpartialBofZ.value.length*targetDimension);
+//
+//    //for(i <- 0 until iterations){
+//      MatrixUtils.matrixMultiply(bcpartialBofZ.value, bcPrex.value, bcpartialBofZ.value.length, targetDimension, globalColCount, blockSize, multiplyResult);
+//    //}
+//
+//    println("Task Index " + index + " Number of Rows " + localRowCount)
+//    return List(localRowCount).iterator;
+//  }
 
-    MMUtils.generatePreX(globalColCount,targetDimension,preX);
-    MMUtils.generateBofZ(localRowCount,globalColCount,partialBofZ);
-
-    //for(i <- 0 until iterations){
-      MatrixUtils.matrixMultiply(partialBofZ, preX, partialBofZ.length, targetDimension, globalColCount, blockSize, multiplyResult);
-    //}
-
-    println("Task Index " + index + " Number of Rows " + localRowCount)
-    return List(localRowCount).iterator;
-  }
-
-  def runTaskCollect(globalColCount: Int, targetDimension: Int, taskRowCounts: Array[Int], blockSize: Int, iterations: Int)(index: Int, iter: Iterator[Int]) : Iterator[Array[Double]] = {
-    val localRowCount = taskRowCounts(index);
-    val preX: Array[Double] = Array.ofDim[Double](globalColCount * targetDimension);
-    val partialBofZ: Array[Array[Double]] = Array.ofDim[Double](localRowCount,globalColCount);;
-    val multiplyResult: Array[Double] = Array.ofDim[Double](partialBofZ.length*targetDimension);
-
-    MMUtils.generatePreX(globalColCount,targetDimension,preX);
-    MMUtils.generateBofZ(localRowCount,globalColCount,partialBofZ);
-
-    //for(i <- 0 until iterations){
-    MatrixUtils.matrixMultiply(partialBofZ, preX, partialBofZ.length, targetDimension, globalColCount, blockSize, multiplyResult);
-    //}
-
-    println("Task Index " + index + " Number of Rows " + localRowCount)
-    return List(multiplyResult).iterator;
-  }
+//  def runTaskCollect(globalColCount: Int, targetDimension: Int, taskRowCounts: Array[Int], blockSize: Int, iterations: Int)(index: Int, iter: Iterator[Int]) : Iterator[Array[Double]] = {
+//    val localRowCount = taskRowCounts(index);
+//
+//    val multiplyResult: Array[Double] = Array.ofDim[Double](bcpartialBofZ.value.length*targetDimension);
+//
+//    //for(i <- 0 until iterations){
+//    MatrixUtils.matrixMultiply(bcpartialBofZ.value, bcPrex.value, bcpartialBofZ.value.length, targetDimension, globalColCount, blockSize, multiplyResult);
+//    //}
+//
+//    println("Task Index " + index + " Number of Rows " + localRowCount)
+//    return List(multiplyResult).iterator;
+//  }
 
   def collectResults(main: Array[Double],next: Array[Double]) : Array[Double] = {
     return main ++ next;
